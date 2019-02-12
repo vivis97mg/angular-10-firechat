b9d052748ba11f716ee382136d906b13d715d823
vivis97mg <vivis97morego@gmail.com>
2019-02-12 18:52:03 -0500

    Publicando en github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDZvxbiVKrF0eZ1QoQbxcySrjQWLu7qsfs",
+    authDomain: "firechat-e355b.firebaseapp.com",
+    databaseURL: "https://firechat-e355b.firebaseio.com",
+    projectId: "firechat-e355b",
+    storageBucket: "firechat-e355b.appspot.com",
+    messagingSenderId: "690030238646"
+  }
 };
