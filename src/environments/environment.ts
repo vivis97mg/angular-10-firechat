40e60bff631546dba2fbd9bacc066515919316c2
vivis97mg <vivis97morego@gmail.com>
2019-02-12 18:59:39 -0500

    Cambio en ambiente a producción

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyDZvxbiVKrF0eZ1QoQbxcySrjQWLu7qsfs",
     authDomain: "firechat-e355b.firebaseapp.com",
