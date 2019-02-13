897b89944100fb8dc4a7d3598a003e0ec829a4c3
vivis97mg <vivis97morego@gmail.com>
2019-02-12 19:37:39 -0500

    Corregir la autorización del dominio para firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: "AIzaSyDZvxbiVKrF0eZ1QoQbxcySrjQWLu7qsfs",
-    authDomain: "https://vivis97mg.github.io/angular-10-firechat/",
+    authDomain: "firechat-e355b.firebaseapp.com",
     databaseURL: "https://firechat-e355b.firebaseio.com",
     projectId: "firechat-e355b",
     storageBucket: "firechat-e355b.appspot.com",
